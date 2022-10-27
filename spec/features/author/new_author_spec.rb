541554bf9787b36e1a6eef97eafca6bc0104b5d2
notacop92 <62717141+notacop92@users.noreply.github.com>
2022-10-27 12:32:10 +0200

    :test_tube: add red form test

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,21 @@ require 'rails_helper'
      # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
      visit new_author_path
    end
+
+   it "should display a html form with first name, last name and homepage field" do
+    visit new_author_path
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
+   end
+   
+   it "save the the submited form" do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: 'Edsger'
+    page.fill_in 'author[last_name]', with: 'Dijkstra'
+    page.fill_in 'author[homepage]', with: 'https://en.wikipedia.org/wiki/Edsger_W._Dijkstra'
+    find('input[type="submit"]').click
+    expect((Author.find_by last_name: 'Dijkstra').first_name).to eq('Edsger')
+   end
+
 end
