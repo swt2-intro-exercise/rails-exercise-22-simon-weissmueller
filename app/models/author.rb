b717e73cd5ace7c9363347f9c0ad392d6defe436
notacop92 <62717141+notacop92@users.noreply.github.com>
2022-10-27 11:59:16 +0200

    :card_file_box: generate author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,15 @@
 class Author < ApplicationRecord
+    attr_accessor :first_name, :last_name, :homepage
+    
+    def name
+        return first_name + ' ' + last_name
+    end
+
+
+    # def initialize(first_name, last_name, homepage)
+    #     @first_name = first_name
+    #     @last_name = last_name
+    #     @homepage = homepage
+    # end
+
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,9 +1,12 @@
 
 describe "New author model", type: :model do
-    it "should have first_name eq. to 'Alan', last_name eq. to 'Turing', homepage eq. to 'http://wikipedia.org/Alan_Turing', name eq. to 'Alan Turing'" do
-        author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
+    it "should have first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing', name eq. to 'Alan Turing'" do
+        author = Author.new()
+        author.first_name = 'Alan'
         expect(author.first_name).to eq('Alan')
+        author.last_name = 'Turing'
         expect(author.last_name).to eq('Turing')
+        author.homepage = 'http://wikipedia.org/Alan_Turing'
         expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
         expect(author.name).to eq('Alan Turing')
     end
