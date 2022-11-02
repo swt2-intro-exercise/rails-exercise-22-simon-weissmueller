53f221507a1cd3da90ad96cb7d053f8d40e498b5
notacop92 <62717141+notacop92@users.noreply.github.com>
2022-11-02 13:58:25 +0100

    add edit link to index.html.erb

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -6,8 +6,11 @@ describe "Author index page", type: :feature do
         expect(page).to have_text("Homepage")
         expect(page).to have_link 'New', href: new_author_path
     end
+    #geht das auch anders
     it "should have link to Edit Author Page" do
-    visit authors_path
-    expect(page).to have_link 'Edit', href: edit_author_path
+        @author = Author.new(first_name: "Alan", last_name: "Turing", homepage: 'http://wikipedia.org/Alan_Turing')
+        @author.save
+        visit authors_path
+        expect(page).to have_link 'Edit', href: edit_author_path(@author)
     end
 end
