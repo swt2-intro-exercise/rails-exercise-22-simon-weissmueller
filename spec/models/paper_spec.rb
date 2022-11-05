53f9180dbb16261e92121da66701317af60da112
notacop92 <62717141+notacop92@users.noreply.github.com>
2022-11-05 19:08:38 +0100

    add papers model validation test

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not accept empty inputs" do 
+    @paper = Paper.new(title: 'Title',venue: 'Venue',year: '2000')
+    expect(@paper.save()).to eq(true)
+    @paper = Paper.new(title: 'Title',venue: '',year: '2000')
+    expect(@paper.save()).to eq(false)
+    @paper = Paper.new(title: '',venue: 'Venue',year: '2000')
+    expect(@paper.save()).to eq(false)
+    @paper = Paper.new(title: 'Title',venue: 'Venue',year: '')
+    expect(@paper.save()).to eq(false)
+  end
+  it "should only accept numerical data as year attribut" do 
+    @paper = Paper.new(title: 'Title',venue: 'Venue',year: '2000')
+    expect(@paper.save()).to eq(true)
+    @paper = Paper.new(title: 'Title',venue: 'Venue',year: 'Year2000')
+    expect(@paper.save()).to eq(false)
+  end
 end
